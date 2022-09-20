8cae65a0c06320f210f4631d12cb342ae168520d
Mel <brandon.melend14@gmail.com>
2022-09-20 17:24:17 -0400

    Completed exercsise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(currentArray,...itemsToRemove) {
+
+    for(let i = 0; i<itemsToRemove.length;i++){
+        // console.log(i)
+        for(let j=0;j<currentArray.length;j++){
+            // console.log(i,j)
+            if (itemsToRemove[i]===currentArray[j]){
+                currentArray.splice(j,1)
+                // console.log(currentArray)
+            }
+        }
+    }
+    return currentArray
 
 };
 
