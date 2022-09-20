8c9974e8adf4246c80b143e91d194b52c970aac0
Mel <brandon.melend14@gmail.com>
2022-09-20 16:56:36 -0400

    Completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverseWord = ""
+    let wordArray = word.split("")
+    for(let i =0; i<word.length;i++){
+        reverseWord += wordArray.pop(-1)
+    }
+    return reverseWord
 };
 
 // Do not edit below this line
