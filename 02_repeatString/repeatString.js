342183dce8fc7cd78a19fde01206eb1f73813473
Mel <brandon.melend14@gmail.com>
2022-09-20 16:47:37 -0400

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word,num) {
+    let repeatWord = ""
+    if(num<0){
+        return "ERROR"
+    }
+    for(let i =0; i < num; i++){
+        repeatWord += word
+    }
+    return repeatWord
 };
 
 // Do not edit below this line
