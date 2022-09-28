68322dc8a5d7c65180bddc718e0ad48ef88607f2
Mel <brandon.melend14@gmail.com>
2022-09-28 16:24:35 -0400

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let parseNum = parseInt(number)
+    if(parseNum<0){
+        return "OOPS";
+    }
+    if(parseNum===0) return 0;
+    
+    let fibNum = Math.round(((1.618034)**(parseNum) - (1 - 1.618034)** (parseNum)) / Math.sqrt(5))
+  
+    return fibNum;
 };
 
 // Do not edit below this line
