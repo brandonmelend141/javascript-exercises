8e8197944dd997787e45b885080922dc76c15cbf
Mel <brandon.melend14@gmail.com>
2022-09-28 15:19:37 -0400

    Completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  const addNum = numbers.reduce((total,number)=>total + number,0);
+  return addNum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  const timesNum = numbers.reduce((total,number)=>total * number,1);
+  return timesNum;
 };
 
-const power = function() {
-	
+const power = function(num,power) {
+	return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+  let total = value 
+  if(value === 0){
+    return 1;
+  }
+  
+	for(let i = 1; i < value;i++){
+    total *= i;
+  }
+  return total
 };
 
 // Do not edit below this line
