cce3dcc1175e63b4402318e4baf37af175321012
Mel <brandon.melend14@gmail.com>
2022-09-28 15:39:33 -0400

    Completed exercise 09 by using solution from branch, need to learn regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    // const wordArray = word.split('');
+    // const reverseWord = wordArray.reverse().join('');
+    // console.log(reverseWord);
+    // return reverseWord === word
 
+    // solution from branch, learn REGEX as a goal 
+    processedString = word.toLowerCase().replace(/[^a-z]/g, "");
+    console.log(processedString)
+    return (
+        processedString
+        .split("")
+        .reverse()
+        .join("") == processedString
+    );
 };
 
 // Do not edit below this line
